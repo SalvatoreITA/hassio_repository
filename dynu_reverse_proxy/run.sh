150358aeac69b21ab854d02905220838097ea706
Salvatore Lentini <74460788+SalvatoreITA@users.noreply.github.com>
2023-11-30 18:30:38 +0100

    Update run.sh

--- dynu_reverse_proxy/run.sh
+++ dynu_reverse_proxy/run.sh
@@ -14,7 +14,7 @@ check_ssl () {
    if [ -z "${CERTS}" ]
    then
      bashio::log.info "no certificate found try to generate it..."
-     certbot --nginx --email admin@localhost.it --domain ${DOMAIN} -n --agree-tos --config-dir /ssl/generic/ > /dev/null
+     certbot --nginx --email admin@localhost.it --domain ${DOMAIN} -n --agree-tos --config-dir /ssl/mattiols/ > /dev/null
      if [ $? != 0 ]
       then
 	bashio::log.error "ERROR can't validate new certificate"
@@ -33,7 +33,7 @@ check_ssl () {
      sed -i -e "s~/etc/ssl/nginx/localhost.crt~$cert~g" /etc/nginx/nginx.conf &> /dev/null
      kill -15 "${API_PID[@]}"
      wait "${API_PID[@]}"
-     bashio::log.info "Start Dynu Reverse Proxy..."
+     bashio::log.info "Start Mattiols Reverse Proxy..."
      /usr/sbin/nginx & > /dev/null
      API_PID+=($!)
    fi
@@ -50,7 +50,7 @@ check_ssl
 
 if ! grep -q "certbot" /etc/crontabs/root
  then
-   echo "0 12 * * * /usr/bin/certbot renew --quiet --config-dir /ssl/dynu/ ; nginx -s reload 2>&1 >> /dev/null" >> /etc/crontabs/root
+   echo "0 12 * * * /usr/bin/certbot renew --quiet --config-dir /ssl/mattiols/ ; nginx -s reload 2>&1 >> /dev/null" >> /etc/crontabs/root
 fi
 
 if ! grep -q "noipy" /etc/crontabs/root
@@ -64,7 +64,7 @@ API_PID+=($!)
 
 # Check IP
 bashio::log.info "Check IP Provider"
-/usr/bin/noipy -u ${TOKEN} -n ${DOMAIN} --provider dynu
+/usr/bin/noipy -u ${TOKEN} -n ${DOMAIN} --provider duck
 
 function stop_api() {
     bashio::log.info "Kill Processes..."
